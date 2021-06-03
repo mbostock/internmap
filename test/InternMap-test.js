0fa5d173792c6ed79df90cdd7eb244e89488e3df
Mike Bostock <mbostock@gmail.com>
2021-06-03 16:13:06 -0700

    type: module

--- test/InternMap-test.js
+++ test/InternMap-test.js
@@ -1,5 +1,5 @@
-const {InternMap} = require("../");
-const tape = require("tape-await");
+import {InternMap} from "../src/index.js";
+import assert from "assert";
 
 class Value {
   constructor(value) {
@@ -10,12 +10,12 @@ class Value {
   }
 }
 
-tape("InternMap is an instanceof Map", test => {
-  test.ok(new InternMap() instanceof Map);
+it("InternMap is an instanceof Map", () => {
+  assert(new InternMap() instanceof Map);
 });
 
-tape("InternMap(null) returns an empty set", test => {
-  test.equal(new InternMap(null).size, 0);
+it("InternMap(null) returns an empty set", () => {
+  assert.strictEqual(new InternMap(null).size, 0);
 });
 
 basicTest("strings", "a", "b", "c");
@@ -24,31 +24,31 @@ basicTest("dates", new Date(Date.UTC(2001, 0, 1)), new Date(Date.UTC(2002, 0, 1)
 basicTest("custom", new Value(1), new Value(2), new Value(3));
 
 function basicTest(name, a, b, c) {
-  tape(`InternMap(${name})`, test => {
+  it(`InternMap(${name})`, () => {
     const entries = [[a, 1], [b, 2]];
     const map = new InternMap([[a, 1], [b, 1], [b, 2]]);
-    test.deepEquals([...map], entries);
-    test.deepEquals([...map.keys()], entries.map(([key]) => key));
-    test.deepEquals([...map.values()], entries.map(([, value]) => value));
-    test.deepEquals([...map.entries()], entries);
-    test.equals(map.has(a), true);
-    test.equals(map.has(b), true);
-    test.equals(map.has(c), false);
-    test.equals(map.get(a), 1);
-    test.equals(map.get(b), 2);
-    test.equals(map.get(c), undefined);
+    assert.deepStrictEqual([...map], entries);
+    assert.deepStrictEqual([...map.keys()], entries.map(([key]) => key));
+    assert.deepStrictEqual([...map.values()], entries.map(([, value]) => value));
+    assert.deepStrictEqual([...map.entries()], entries);
+    assert.strictEqual(map.has(a), true);
+    assert.strictEqual(map.has(b), true);
+    assert.strictEqual(map.has(c), false);
+    assert.strictEqual(map.get(a), 1);
+    assert.strictEqual(map.get(b), 2);
+    assert.strictEqual(map.get(c), undefined);
   });
 }
 
-tape(`InternMap(entries) returns the first of heterogeneous types`, test => {
+it(`InternMap(entries) returns the first of heterogeneous types`, () => {
   const x = Date.UTC(2001, 0, 1);
   const d = new Date(x);
   const map1 = new InternMap([[x, "number"], [d, "date"]]);
-  test.equals(map1.get(x), "date");
-  test.equals(map1.get(d), "date");
-  test.deepEquals([...map1.entries()], [[x, "date"]]);
+  assert.strictEqual(map1.get(x), "date");
+  assert.strictEqual(map1.get(d), "date");
+  assert.deepStrictEqual([...map1.entries()], [[x, "date"]]);
   const map2 = new InternMap([[d, "date"], [x, "number"]]);
-  test.equals(map2.get(x), "number");
-  test.equals(map2.get(d), "number");
-  test.deepEquals([...map2.entries()], [[d, "number"]]);
+  assert.strictEqual(map2.get(x), "number");
+  assert.strictEqual(map2.get(d), "number");
+  assert.deepStrictEqual([...map2.entries()], [[d, "number"]]);
 });
--- test/InternSet-test.js
+++ test/InternSet-test.js
@@ -1,5 +1,5 @@
-const {InternSet} = require("../");
-const tape = require("tape-await");
+import assert from "assert";
+import {InternSet} from "../src/index.js";
 
 class Value {
   constructor(value) {
@@ -10,12 +10,12 @@ class Value {
   }
 }
 
-tape("InternSet is an instanceof Set", test => {
-  test.ok(new InternSet() instanceof Set);
+it("InternSet is an instanceof Set", () => {
+  assert(new InternSet() instanceof Set);
 });
 
-tape("InternSet(null) returns an empty set", test => {
-  test.equal(new InternSet(null).size, 0);
+it("InternSet(null) returns an empty set", () => {
+  assert.strictEqual(new InternSet(null).size, 0);
 });
 
 basicTest("strings", "a", "b", "c");
@@ -24,27 +24,27 @@ basicTest("dates", new Date(Date.UTC(2001, 0, 1)), new Date(Date.UTC(2002, 0, 1)
 basicTest("custom", new Value(1), new Value(2), new Value(3));
 
 function basicTest(name, a, b, c) {
-  tape(`InternSet(${name})`, test => {
+  it(`InternSet(${name})`, () => {
     const values = [a, b];
     const set = new InternSet([a, b, b]);
-    test.deepEquals([...set], values);
-    test.deepEquals([...set.values()], values);
-    test.deepEquals([...set.entries()], values.map(value => [value, value]));
-    test.equals(set.has(a), true);
-    test.equals(set.has(b), true);
-    test.equals(set.has(c), false);
+    assert.deepStrictEqual([...set], values);
+    assert.deepStrictEqual([...set.values()], values);
+    assert.deepStrictEqual([...set.entries()], values.map(value => [value, value]));
+    assert.strictEqual(set.has(a), true);
+    assert.strictEqual(set.has(b), true);
+    assert.strictEqual(set.has(c), false);
   });
 }
 
-tape(`InternSet(values) returns the first of heterogeneous types`, test => {
+it(`InternSet(values) returns the first of heterogeneous types`, () => {
   const x = Date.UTC(2001, 0, 1);
   const d = new Date(x);
   const set1 = new InternSet([x, d]);
-  test.equals(set1.has(x), true);
-  test.equals(set1.has(d), true);
-  test.deepEquals([...set1.values()], [x]);
+  assert.strictEqual(set1.has(x), true);
+  assert.strictEqual(set1.has(d), true);
+  assert.deepStrictEqual([...set1.values()], [x]);
   const set2 = new InternSet([d, x]);
-  test.equals(set2.has(x), true);
-  test.equals(set2.has(d), true);
-  test.deepEquals([...set2.values()], [d]);
+  assert.strictEqual(set2.has(x), true);
+  assert.strictEqual(set2.has(d), true);
+  assert.deepStrictEqual([...set2.values()], [d]);
 });
